78508dfea331aad87c379756879a9dfc884a29f5
KageDavin <kirbypinkham.journey@gmail.com>
2025-08-27 10:07:23 +0000

    updated logic.js and created index.html -added humanChoice logic to js file -created index.htmnl file for testing -Comparison win-loss logic still needed

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-// getComputerChoice
+// #### getComputerChoice ####
 // Use Math.random() for choice of rock, scissors, paper
 const min = 10;
 const max = 40;
@@ -20,3 +20,67 @@ function getComputerChoice() {
 
 const compChoice = getComputerChoice();
 console.log('Computer chose: ' + compChoice);
+
+
+
+
+// #### getHumanChoice ####
+// use Prompt for input
+
+
+function getHumanChoice() {
+  let showWeapons;
+
+ while (true) {
+    showWeapons = prompt("Choose your weapon: 1-ROCK, 2-SCISSORS, or 3-PAPER.");
+
+    if (showWeapons === null) {
+      console.log("Human did not select a weapon. No Battle");
+      return null; 
+    }
+
+    const selectWeapon = showWeapons * 1;
+
+    if (selectWeapon === 1 || selectWeapon === 2 || selectWeapon === 3) {
+     return selectWeapon;
+    } else {
+      alert("Choose a valid weapon: 1-ROCK, 2-SCISSORS, 3-PAPER");
+    }
+  }
+}
+
+
+function getWeapon(weaponChoice) {
+  switch (weaponChoice) {
+    case 1:
+      return 'ROCK';
+    case 2:
+      return 'SCISSORS';
+    case 3:
+      return 'PAPER';
+    default:
+      return null;
+  }
+}
+
+
+const giveWeapon = getHumanChoice();
+
+if (giveWeapon !== null) {
+  const humanChoice = getWeapon(giveWeapon);
+  console.log(`The user chose: ${humanChoice}`);
+  
+}
+
+// Computer vs Human Comparison
+
+function playRound(humanChoice, computerChoice) {
+  
+
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
